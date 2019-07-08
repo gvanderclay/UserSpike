1c66231aede93217a5c178029b6a9b1f096c98ee
Gage Vander Clay <gvanderclay@gmail.com>
2019-07-08 17:12:22 -0400

    more stuff

--- App.tsx
+++ App.tsx
@@ -1,99 +1,154 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * 
+ *
  * Generated with the TypeScript template
  * https://github.com/emin93/react-native-template-typescript
  *
  * @format
  */
 
-import React, {Fragment} from 'react';
+import React, { Fragment, useEffect, useState } from "react";
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
-  StatusBar,
-} from 'react-native';
+  StatusBar
+} from "react-native";
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import * as _ from "lodash";
+import SQLite from "react-native-sqlite-storage";
+import Knex from "knex";
+
+type User = {
+  gender: string;
+  name: {
+    first: string;
+    last: string;
+  };
+  nat: string;
+};
 
 const App = () => {
+  const [title, setTitle] = useState("waiting");
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    console.log("useEffect");
+    SQLite.DEBUG(true);
+    SQLite.enablePromise(true);
+
+    SQLite.openDatabase({
+      name: "TestDatabase",
+      location: "default"
+    }).then(db => {
+      db.transaction(transaction => {
+        transaction.executeSql("DROP TABLE IF EXISTS Users;");
+        transaction.executeSql("DROP TABLE IF EXISTS Facets;");
+        transaction.executeSql("DROP TABLE IF EXISTS FacetValues;");
+        transaction.executeSql("DROP TABLE IF EXISTS UserFacets;");
+        transaction.executeSql(`
+          CREATE TABLE Users(
+            user_id INTEGER PRIMARY KEY NOT NULL,
+            name TEXT
+          );
+        `);
+
+        transaction.executeSql(`
+          CREATE TABLE Facets(
+            facet_id INTEGER PRIMARY KEY NOT NULL,
+            name TEXT NOT NULL UNIQUE
+          );
+        `);
+
+        transaction.executeSql(`
+          CREATE TABLE FacetValues(
+            facet_value_id INTEGER PRIMARY KEY NOT NULL,
+            facet_id INTEGER,
+            facet_value TEXT NOT NULL,
+            FOREIGN KEY(facet_id) REFERENCES Facets(facet_id)
+          );
+        `);
+
+        transaction.executeSql(`
+          CREATE TABLE UserFacets(
+            user_id INTEGER NOT NULL,
+            facet_value_id INTEGER NOT NULL,
+            FOREIGN KEY(facet_value_id) REFERENCES FacetValues(facet_value_id)
+          );
+        `);
+
+        transaction.executeSql(`
+            INSERT into Facets (name)
+            VALUES ('gender');
+        `);
+
+        transaction.executeSql(`
+            INSERT into Facets (name)
+            VALUES ('nationality');
+        `);
+      })
+        .then(transaction => {
+          fetch("https://randomuser.me/api/?results=50").then(response => {
+            response.json().then(data => {
+              console.log(data);
+
+              const parsedNames = data.results.map(
+                (user: User) => user.name.first
+              );
+              transaction.executeSql(
+                `
+              INSERT into USERS (name)
+              VALUES(?);
+            `,
+                [parsedNames]
+              );
+              const parsedGenders = _.uniq(
+                data.results.map((user: User) => user.gender)
+              );
+              transaction
+                .executeSql(
+                  `SELECT * from Facets facet where facet.name = 'gender';`
+                )
+                .then(([tx, result]) => {
+                  if (result.rows.length > 0) {
+                    let genderFacet = result.rows.item(0);
+                    transaction.executeSql(
+                      `
+                  INSERT into FacetValues (facet_id, facet_value)
+                  VALUES(${genderFacet.facet_id}, ?);
+                `,
+                      [parsedGenders]
+                    );
+                  }
+                });
+              const parsedNationalities = _.uniq(
+                data.results.map((user: User) => user.nat)
+              );
+            });
+          });
+        })
+        .then(() => setTitle("Done"))
+        .catch(e => setTitle(e));
+    });
+  }, []);
+
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          <Text>{title}</Text>
+          {data.map(user => (
+            <Text>{user}</Text>
+          ))}
         </ScrollView>
       </SafeAreaView>
     </Fragment>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
