8d529853d28cfd6a224ae416193645938eea2c42
Gage Vander Clay <gvanderclay@gmail.com>
2019-07-09 15:39:54 -0400

    refactor it

--- App.tsx
+++ App.tsx
@@ -34,66 +34,24 @@ const App = () => {
   const [title, setTitle] = useState("waiting");
   const [data, setData] = useState<User[]>([]);
   useEffect(() => {
-    console.log("useEffect");
-    SQLite.DEBUG(true);
-    SQLite.enablePromise(true);
-
-    SQLite.openDatabase({
-      name: "TestDatabase",
-      location: "default"
-    }).then(db => {
-      dropTables(db)
-        .then(() => {
-          createTables(db).then(() => {
-            seedFacets(db).then(() => {
-              getUserData().then(result => {
-                const users = result[0];
-                const facets = result[1];
-                insertUsers(db, users).then(() => {
-                  getFacetIds(db).then(facetIds => {
-                    const data: { [key: string]: [string[], string] } = {
-                      gender: [facets["gender"], facetIds["gender"]],
-                      nat: [facets["nat"], facetIds["nat"]]
-                    };
-                    insertFacetValues(db, data).then(() => {
-                      db.executeSql(
-                        `
-                        SELECT * FROM FacetValues;
-                      `
-                      ).then(([result]) => {
-                        const items: string[] = _.map(
-                          _.range(result.rows.length),
-                          x => result.rows.item(x).facet_value
-                        );
-                        console.log(items);
-                        setData(items);
-                      });
-                    });
-                  });
-                });
-              });
-            });
-          });
-        })
-        .then(() => {
-          setTitle("Done");
-        })
-        .catch(e => setTitle(e));
-    });
+    async function foo() {
+      try {
+        await handleUpdates(setData);
+        setTitle("Done");
+      } catch (e) {
+        setTitle("AHHH");
+      }
+    }
+    foo();
   }, []);
-
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView contentInsetAdjustmentBehavior="automatic">
           <Text>{title}</Text>
-          {data.map(user => (
-            <View>
-              <Text>{user.name}</Text>
-              <Text>{user.gender}</Text>
-              <Text>{user.nat}</Text>
-            </View>
+          {data.map((data, index) => (
+            <Text key={index}>{JSON.stringify(data)}</Text>
           ))}
         </ScrollView>
       </SafeAreaView>
@@ -140,6 +98,7 @@ function createTables(db: SQLite.SQLiteDatabase): Promise<SQLite.Transaction> {
         user_id INTEGER NOT NULL,
         facet_value_id INTEGER NOT NULL,
         FOREIGN KEY(facet_value_id) REFERENCES FacetValues(facet_value_id)
+        FOREIGN KEY(user_id) REFERENCES Users(user_id)
       );
     `);
   });
@@ -163,8 +122,6 @@ function getUserData(): Promise<[User[], { [key: string]: string[] }]> {
   return new Promise((res, rej) => {
     fetch("https://randomuser.me/api/?results=50").then(response => {
       response.json().then(data => {
-        console.log(data);
-
         const parsedGenders: string[] = _.uniq(
           data.results.map((user: User) => user.gender)
         );
@@ -173,24 +130,10 @@ function getUserData(): Promise<[User[], { [key: string]: string[] }]> {
           data.results.map((user: User) => user.nat)
         );
 
-        res([data.results as User[], { gender: parsedGenders, nat: parsedNationalities }]);
-      });
-    });
-  });
-}
-
-function getFacetIds(
-  db: SQLite.SQLiteDatabase
-): Promise<{ [key: string]: string }> {
-  return new Promise((res, rej) => {
-    db.transaction(transaction => {
-      transaction.executeSql(`SELECT * from Facets;`).then(([tx, result]) => {
-        const items = _.map(_.range(result.rows.length), x =>
-          result.rows.item(x)
-        );
-        const genderId = _.find(items, item => item.name === "gender");
-        const natId = _.find(items, item => item.name === "nat");
-        res({ genderId, natId });
+        res([
+          data.results as User[],
+          { gender: parsedGenders, nat: parsedNationalities }
+        ]);
       });
     });
   });
@@ -198,57 +141,100 @@ function getFacetIds(
 
 function insertFacetValues(
   db: SQLite.SQLiteDatabase,
-  facetValues: { [key: string]: [string[], string] }
+  facetValues: { [key: string]: string[] }
 ): Promise<SQLite.Transaction> {
   return db.transaction(transaction => {
-    _.forEach(facetValues["gender"][0], value => {
+    _.forEach(facetValues["gender"], value => {
       transaction.executeSql(
         `
           INSERT into FacetValues (facet_id, facet_value)
-          VALUES(?, ?);
+          SELECT facet_id, ?
+          FROM Facets as f where f.name = 'gender';
         `,
-        [facetValues["gender"][1], value]
+        [value]
       );
     });
 
-    _.forEach(facetValues["nat"][0], value => {
+    _.forEach(facetValues["nat"], value => {
       transaction.executeSql(
         `
           INSERT into FacetValues (facet_id, facet_value)
-          VALUES(?, ?);
+          SELECT facet_id, ?
+          FROM Facets as f where f.name = 'nat';
         `,
-        [facetValues["nat"][1], value]
+        [value]
       );
     });
   });
 }
 
-function insertUsers(db: SQLite.SQLiteDatabase, users: User[]) {
-  return db.transaction(transaction => {
+async function insertUsers(
+  db: SQLite.SQLiteDatabase,
+  users: User[]
+): Promise<void> {
+  await db.transaction(transaction => {
     _.forEach(users, user => {
       transaction.executeSql(
         `
           INSERT into Users (name)
           VALUES(?);
         `,
-        [user.name]
+        [user.name.first],
+        (x, y) => console.log(y.rows)
       );
     });
-  }).then(() => {
-    db.transaction(transaction => {
-      _.forEach(users, user => {
-        transaction.executeSql(
-          `
-            INSERT INTO UserFacets(user_id, facet_value_id)
-            VALUES (?, fId)
-            SELECT facet_value_id FROM FacetValues fv
-            JOIN Facets f ON fv.facet_id = f.facet_id;
-          `
-        );
-      });
+  });
+  await db.transaction(transaction => {
+    _.forEach(users, user => {
+      // transaction.executeSql(
+      //   `
+      //       INSERT INTO UserFacets(user_id, facet_value_id)
+      //       SELECT facet_value_id, user_id
+      //       FROM FacetValues fv
+      //       JOIN Facets f ON fv.facet_id = f.facet_id;
+      //     `
+      // );
     });
   });
 }
+
+const handleUpdates = async (cb: any) => {
+  const db = await SQLite.openDatabase({
+    name: "TestDatabase",
+    location: "default"
+  });
+  try {
+    await dropTables(db);
+    await createTables(db);
+    await seedFacets(db);
+    const [users, facets] = await getUserData();
+    await insertUsers(db, users);
+    await insertFacetValues(db, facets);
+    try {
+      db.transaction(async tx => {
+        tx.executeSql(
+          `SELECT * FROM Users;`,
+          undefined,
+          (tx, queryResult) => {
+            const items: string[] = _.map(_.range(queryResult.rows.length), x =>
+              queryResult.rows.item(x)
+            );
+            cb(items);
+          },
+          (tx, error) => {
+            console.error(error);
+          }
+        );
+      });
+    } catch (e) {
+      console.error("HERE", e);
+      throw e;
+    }
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
 
 const styles = StyleSheet.create({});
 
